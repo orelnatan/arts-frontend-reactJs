0767118719c7a898e33cfbf825db2a698aa056f0
Orel Natan <oreln@bizibox.biz>
2026-04-17 15:24:56 +0300

    Arts Frontend - initial commit - infrastructure and design system

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss'
+
+import Login from './features/auth/pages/login/Login'
+
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
---
+++ src/features/auth/pages/login/Login.tsx
@@ -0,0 +1,9 @@
+import './Login.scss'
+
+export default function Login() {
+  return (
+    <>
+      This is Login page component
+    </>
+  )
+}
